8242659ae96ec3f12b2369a1c8c565627d39539e
Kaan Kaplan <kkaplankaan@gmail.com>
2024-12-14 16:27:09 +0300

    Npc Interaction
    pelinsucod

--- Kekolomanya/Assets/Pelinsu/PelinSu.cs
+++ Kekolomanya/Assets/Pelinsu/PelinSu.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PelinSu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject Ýnteractive;
+    public bool Speech = false, Once = true;
+    public string[] NpcSpeech;
+    public Sprite npcSprite, Player;
+    public int SpeechNumber;
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            Speech = true;
+            Ýnteractive.SetActive(true);
+        }
     }
+    public void Update()
+    {
+        if (Speech && Input.GetKeyDown(KeyCode.E) && Once) 
+        {
+            Once = false;
 
-    // Update is called once per frame
-    void Update()
+            StartCoroutine(Chat());
+        }
+    }
+    IEnumerator Chat()
     {
         
+        //speechleri yazarsýn
+       
+        char[] stringsx = NpcSpeech[SpeechNumber].ToCharArray();
+
+        StartCoroutine(SpeechCode.SpeechMan.SpeechRoutine(NpcSpeech[SpeechNumber], npcSprite));
+
+        SpeechNumber += 1;
+
+        yield return new WaitForSeconds(stringsx.Length * 0.05f + 2.5f);
+        Once = true;
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Speech = true;
+            Ýnteractive.SetActive(true);
+        }
     }
 }
